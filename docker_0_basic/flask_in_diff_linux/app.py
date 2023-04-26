a2d88588c72fd47c21bc909b25576f9cec1ff656
davidkhckhc <76805794+davidkhckhc@users.noreply.github.com>
2023-04-26 11:59:30 +0800

    commit1

--- docker_0_basic/flask_in_diff_linux/app.py
+++ docker_0_basic/flask_in_diff_linux/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World! I am in HKBNJOS container'
+    return 'Hello World! I am in JOS container'
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080, debug=True)
