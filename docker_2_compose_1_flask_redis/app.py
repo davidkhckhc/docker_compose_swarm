30b1354dd646bec97791a12fe32e788651c624f6
davidkhckhc <76805794+davidkhckhc@users.noreply.github.com>
2023-05-06 14:59:25 +0800

    commit1

--- docker_2_compose_1_flask_redis/app.py
+++ docker_2_compose_1_flask_redis/app.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding=utf-8 -*-
-# 本脚由亁颐堂现任明教教主编写，用于乾颐盾Python课程！
-# 教主QQ:605658506
-# 亁颐堂官网www.qytang.com
-# 教主技术进化论拓展你的技术新边疆
-# https://ke.qq.com/course/271956?tuin=24199d8a
 
 import time
 import redis
@@ -29,4 +22,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello Guy~ I have been seen {} times.\n'.format(count)
