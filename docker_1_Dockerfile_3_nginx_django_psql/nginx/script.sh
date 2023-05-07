c39236afb999c1ef5e12e2123730d36f54129832
davidkhckhc <76805794+davidkhckhc@users.noreply.github.com>
2023-05-07 20:00:46 +0800

    commit1

--- docker_1_Dockerfile_3_nginx_django_psql/nginx/script.sh
+++ docker_1_Dockerfile_3_nginx_django_psql/nginx/script.sh
@@ -1,6 +1,6 @@
 docker build -t collinsctk/nginx .
 
-docker run --name qyt-nginx --network nginx-net -p 80:80 -d collinsctk/nginx
+docker run --name qyt-nginx --network nginx-net -p 8083:80 -d collinsctk/nginx
 
 docker network connect django-net qyt-nginx
 
